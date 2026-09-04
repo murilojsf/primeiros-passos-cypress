dcb566eb2fbe056d3234b442d18cd5a39b07cd6f
murilojsf <murilojosefilho2@gmail.com>
2026-09-04 09:37:13 -0300

    First commit

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,17 @@
+describe('Orange HRM tests', () => {
+  it('Login - Sucess', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get('[name="username"]').type('Admin')
+    cy.get('[name="password"]').type('admin123')
+    cy.get("[type='submit']").click()
+    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+    cy.get('.oxd-topbar-header-breadcrumb-module').contains('Dashboard')
+  })
+  it('Login - Fail', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get('[name="username"]').type('Douglas')
+    cy.get('[name="password"]').type('oi1020s')
+    cy.get("[type='submit']").click()
+    cy.get('.oxd-alert')
+  })
+})
